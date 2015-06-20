940a632272a2672d2cf91ba0936d116987483649
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-20 17:22:56 +0200

    Tweaks

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var ghGot = require('gh-got');
 
 module.exports = function (markdown, cb) {
--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 'use strict';
-
-var fs = require('fs');
 var test = require('ava');
 var githubMarkdownRender = require('./');
 
