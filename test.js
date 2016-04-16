5e672e3b19b09c490b6fbe28422674da381d2684
Andreas Gillström <andreasgillstrom@gmail.com>
2016-04-16 12:39:06 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,24 +1,16 @@
-'use strict';
-var test = require('ava');
-var githubMarkdownRender = require('./');
+import test from 'ava';
+import m from './';
 
-test(function (t) {
-	t.plan(2);
-
-	githubMarkdownRender('**foo**', function (err, data) {
-		t.assert(!err, err);
-		t.assert(data.trim() === '<p><strong>foo</strong></p>', data);
+test('callback', t => {
+	m('**foo**', (err, data) => {
+		t.true(!err, err);
+		t.true(data.trim() === '<p><strong>foo</strong></p>', data);
 	});
 });
 
-test(function (t) {
-	t.plan(1);
+test('stream', t => {
+	const stream = m.stream();
 
-	var stream = githubMarkdownRender.stream();
-
-	stream.on('data', function (data) {
-		t.assert(data.toString().trim() === '<p><strong>foo</strong></p>', data.toString());
-	});
-
+	stream.on('data', data => t.true(data.toString().trim() === '<p><strong>foo</strong></p>', data.toString()));
 	stream.end('**foo**');
 });
