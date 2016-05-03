59f2d54b229009b3321bd8fe4a14f4bee2cd6de7
Kevin Martensson <kevinmartensson@gmail.com>
2016-05-03 13:31:57 +0200

    Improve CLI

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
-const fn = require('./');
+const githubMarkdownRender = require('./');
 
 const cli = meow(`
 	Usage
@@ -9,15 +9,15 @@ const cli = meow(`
 	  $ cat readme.md | github-markdown-render
 `);
 
-if (process.stdin.isTTY) {
-	if (!cli.input[0]) {
-		console.error('Markdown required');
-		process.exit(1);
-	}
-
-	return fn(cli.input[0]).then(res => console.log(res));
+if (!cli.input.length && process.stdin.isTTY) {
+	console.error('Markdown required');
+	process.exit(1);
 }
 
-process.stdin
-	.pipe(fn.stream())
-	.pipe(process.stdout);
+if (cli.input.length) {
+	githubMarkdownRender(cli.input[0]).then(res => console.log(res));
+} else {
+	process.stdin
+		.pipe(githubMarkdownRender.stream())
+		.pipe(process.stdout);
+}
